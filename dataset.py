3076f80ae7fd5463811282f1d5b251775fbb59c3
thangdduong <19522195@gm.uit.edu.vn>
2022-05-18 12:28:31 +0700

    add custom daatset class

---
+++ dataset.py
@@ -0,0 +1,38 @@
+import os
+import string
+import torch
+import torchvision.transforms as T
+from PIL import Image
+from torch.utils.data import Dataset
+
+class CaptchaDataset(Dataset):
+    def __init__(self, dataset_dir, img_size=(64, 128)):
+        self.vocab = string.ascii_lowercase + string.digits
+        self.vocab_size = len(self.vocab)
+        self.char2num = {char: i + 1 for i, char in enumerate(self.vocab)}
+        self.num2char = {label: char for char, label in self.char2num.items()}
+        self.dataset_dir = dataset_dir
+        self.all_imgs = os.listdir(dataset_dir)
+
+        self.transform = T.Compose([
+            T.Resize(img_size),
+            T.ToTensor()
+        ])
+
+    def __getitem__(self, index):
+        image_path = os.path.join(self.dataset_dir, self.all_imgs[index])
+        pil_image = Image.open(image_path).convert('RGB')
+        X = self.transform(pil_image)
+
+        label = self.all_imgs[index].split(".")[0]
+        encode_label = [self.char2num[c] for c in label]
+        y_length = [len(encode_label)]
+
+        y = torch.LongTensor(encode_label)
+        y_length = torch.LongTensor(y_length)
+        
+        return X, y, y_length
+
+    def __len__(self):
+
+        return len(self.all_imgs)
